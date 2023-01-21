e7e9850229f3ce57d8883d2bc6c2fff4318551db
Insia Taha <insia.taha@outlook.com>
2023-01-21 23:37:57 +0500

    get weather info reducer

--- clientapi/src/Store/userreducer.js
+++ clientapi/src/Store/userreducer.js
@@ -3,6 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const initialState={
     isLoggedIn:null,
     userinfo:{},
+    WeatherArr:[],
     responses:"responses"
 
 }
@@ -34,6 +35,32 @@ export const readUser=createAsyncThunk(
     }
 )
 
+export const getWeather=createAsyncThunk(
+    'getWeather',
+    async(data,thunkApi)=>{
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        };
+        const res = await fetch('http://localhost:3001/getWeather',requestOptions)
+        // console.log(res,"response")
+        return res.json();
+    }
+)
+export const getUserDashboard=createAsyncThunk(
+    'getUserDashboard',
+    async(data,thunkApi)=>{
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        };
+        const res = await fetch('http://localhost:3001/getUserDashboard',requestOptions)
+        // console.log(res,"response")
+        return res.json();
+    }
+)
 export const userSlice=createSlice({
     name:'user',
     initialState,
@@ -62,7 +89,16 @@ export const userSlice=createSlice({
             console.log(action.payload,"fullfilled from in GetUSER")
             state.responses=action.payload.message
             state.isLoggedIn=action.payload.uid
+        },
+        [getWeather.fulfilled]:(state,action)=>{
+            console.log(action.payload,"Weather of a city")
+            state.responses=action.payload.message
+            state.WeatherArr=action.payload
+        },
+        [getWeather.rejected]:(state,action)=>{
+            console.log(state,"Got rejected from Getting user in ADDUSER")
         }
+        
     }
 })
 
