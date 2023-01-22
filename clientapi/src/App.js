85940c8e6d429d76fc8326430f2273df37238225
Anum Jawaid <JawaiAnu@corp.contoursoftware.net>
2023-01-22 18:48:37 +0500

    ddsf

--- clientapi/src/App.js
+++ clientapi/src/App.js
@@ -2,14 +2,14 @@ import { useEffect } from 'react';
 import AppRouter from './Router';
 import { store } from './Store/store'
 import { Provider } from 'react-redux'
-// import {io} from "socket.io-client";
-// const socket = io.connect("http://localhost:3001");
+import {io} from "socket.io-client";
+const socket = io.connect("http://localhost:3001");
 function App() {
-    // useEffect(() => {
-    //   socket.on("getvalue", data => {
-    //    console.log(data)
-    //   });
-    // }, []);
+    useEffect(() => {
+      socket.on("getvalue", data => {
+       console.log(data)
+      });
+    }, []);
   return (
     <Provider store={store} >
   <AppRouter />
